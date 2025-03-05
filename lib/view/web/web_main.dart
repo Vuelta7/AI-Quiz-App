00062e18d8be2906cb1f15a2a9ee2072f83e0731
Vuelta7 <markvuelta07@gmail.com>
2025-03-05 15:40:47 +0800

    fix the problem in appbar

--- lib/view/web/web_main.dart
+++ lib/view/web/web_main.dart
@@ -25,216 +25,221 @@ class _WebMainState extends State<WebMain> {
 
   @override
   Widget build(BuildContext context) {
+    double appBarHeight = AppBar().preferredSize.height;
+    double screenHeight = MediaQuery.of(context).size.height;
+    double adjustedHeight = screenHeight - appBarHeight;
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 100,
+        toolbarHeight: appBarHeight,
         centerTitle: true,
         title: Image.asset(
           'assets/logo.png',
-          width: 300,
+          width: 150,
         ),
         backgroundColor: Colors.pink,
       ),
       backgroundColor: Colors.pink,
       body: Center(
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-            ),
-            const Text(
-              'The Best Way To Practice Quiz',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                color: Colors.white,
-                fontFamily: 'PressStart2P',
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Colors.cyan, Colors.pink],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: buildRetroButton('Use Android Application',
-                    const Color.fromARGB(0, 255, 255, 255), () {},
-                    height: 70),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [Colors.pink, Colors.cyan],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(40, 40, 40, 0),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Material(
-                        borderOnForeground: true,
-                        elevation: 2,
-                        borderRadius: BorderRadius.circular(9),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.cyan,
-                            borderRadius: BorderRadius.circular(9),
-                            border: Border.all(
-                              width: 3,
-                              color: Colors.white,
-                            ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: adjustedHeight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'The Best Way To Practice Quiz',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontFamily: 'PressStart2P',
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            colors: [Colors.cyan, Colors.pink],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
                           ),
-                          child: const Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                '_',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                              Divider(
-                                thickness: 3,
-                                color: Colors.white,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(20),
-                                child: Text(
-                                  'Best App to Practice Quiz, and with automatic quiz generator for learners, user friendly experience and customizable interface',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: buildRetroButton('Use Android Application',
+                            const Color.fromARGB(0, 255, 255, 255), () {},
+                            height: 70),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [Colors.pink, Colors.cyan],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(40, 40, 40, 0),
+                        child: Column(
+                          children: [
+                            Material(
+                              borderOnForeground: true,
+                              elevation: 2,
+                              borderRadius: BorderRadius.circular(9),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.cyan,
+                                  borderRadius: BorderRadius.circular(9),
+                                  border: Border.all(
+                                    width: 3,
                                     color: Colors.white,
                                   ),
-                                  textAlign: TextAlign.center,
+                                ),
+                                child: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '_',
+                                      style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Divider(
+                                      thickness: 3,
+                                      color: Colors.white,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(20),
+                                      child: Text(
+                                        'Best App to Practice Quiz, and with automatic quiz generator for learners, user friendly experience and customizable interface',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                            const SizedBox(height: 20),
+                            Material(
+                              elevation: 2,
+                              borderRadius: BorderRadius.circular(8),
+                              child: TextField(
+                                enabled: false,
+                                controller: TextEditingController(
+                                    text: showCursor ? "Learn-N|" : "Learn-N"),
+                                style: const TextStyle(
+                                  fontFamily: 'Arial',
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                                decoration: InputDecoration(
+                                  hintText: 'Type Answer',
+                                  hintStyle: const TextStyle(
+                                    fontFamily: 'PressStart2P',
+                                    color: Colors.white,
+                                  ),
+                                  labelStyle: const TextStyle(
+                                    fontFamily: 'PressStart2P',
+                                    color: Colors.white,
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.cyan,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: const BorderSide(
+                                      color: Colors.white,
+                                      width: 3,
+                                    ),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: const BorderSide(
+                                      color: Colors.white,
+                                      width: 3,
+                                    ),
+                                  ),
+                                  disabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: const BorderSide(
+                                      color: Colors.white,
+                                      width: 3,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: const BorderSide(
+                                      color: Colors.white,
+                                      width: 3,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Icon(
+                                  Icons.arrow_back_rounded,
+                                  size: 45,
+                                  color: Colors.white,
+                                ),
+                                Icon(
+                                  Icons.lightbulb,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                                Icon(
+                                  Icons.arrow_forward_rounded,
+                                  size: 45,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                          ],
                         ),
                       ),
-                      const SizedBox(height: 20),
-                      Material(
-                        elevation: 2,
-                        borderRadius: BorderRadius.circular(8),
-                        child: TextField(
-                          enabled: false,
-                          controller: TextEditingController(
-                              text: showCursor ? "Learn-N|" : "Learn-N"),
-                          style: const TextStyle(
-                            fontFamily: 'Arial',
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                          decoration: InputDecoration(
-                            hintText: 'Type Answer',
-                            hintStyle: const TextStyle(
-                              fontFamily: 'PressStart2P',
-                              color: Colors.white,
-                            ),
-                            labelStyle: const TextStyle(
-                              fontFamily: 'PressStart2P',
-                              color: Colors.white,
-                            ),
-                            filled: true,
-                            fillColor: Colors.cyan,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: const BorderSide(
-                                color: Colors.white,
-                                width: 3,
-                              ),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: const BorderSide(
-                                color: Colors.white,
-                                width: 3,
-                              ),
-                            ),
-                            disabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: const BorderSide(
-                                color: Colors.white,
-                                width: 3,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: const BorderSide(
-                                color: Colors.white,
-                                width: 3,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(
-                            Icons.arrow_back_rounded,
-                            size: 45,
-                            color: Colors.white,
-                          ),
-                          Icon(
-                            Icons.lightbulb,
-                            size: 30,
-                            color: Colors.white,
-                          ),
-                          Icon(
-                            Icons.arrow_forward_rounded,
-                            size: 45,
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Colors.pink, Colors.cyan],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: buildRetroButton('Use Website Application',
-                    const Color.fromARGB(0, 255, 255, 255), () {},
-                    height: 50),
+              const SizedBox(
+                height: 20,
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [Colors.pink, Colors.cyan],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: buildRetroButton('Use Website Application',
+                      const Color.fromARGB(0, 255, 255, 255), () {},
+                      height: 50),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
