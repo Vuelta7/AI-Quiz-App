bdee340a04e56e9b4fa6538731c854eb0f2b67fc
Vuelta7 <markvuelta07@gmail.com>
2025-03-31 22:47:45 +0800

    web update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2886297374"
+    serviceWorkerVersion: "3203974380"
   }
 });
